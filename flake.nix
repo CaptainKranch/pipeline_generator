afa99495f3a7e3723c325dae68b2b3149352f823
danielgm <daniel.garcia@ezlo.com>
2024-08-31 12:46:31 -0500

    Update: Working flake version!

--- flake.nix
+++ flake.nix
@@ -1,14 +1,29 @@
 {
-  description = "my project description";
+  description = "A Nix-flake-based Python development environment";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ python312 ] ++
+          (with pkgs.python312Packages; [ 
+            pandas 
+            requests 
+            polars
+            duckdb
+          ]);
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.python312 ]}:$LD_LIBRARY_PATH"
+            # Optionally start nushell, uncomment the next line if needed:
+            exec ${pkgs.nushell}/bin/nu
+          '';
+        };
+      }
+    );
 }
